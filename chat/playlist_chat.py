851cc798f73871682536b99e3279cef37122a632
alicia <agomep03@estudiantes.unileon.es>
2024-11-29 10:45:40 +0100

    basic functional azure chat

--- chat/playlist_chat.py
+++ chat/playlist_chat.py
@@ -2,16 +2,25 @@ import sys
 import os
 
 from openai import AzureOpenAI
-from config import AZURE_OPENAI_API_KEY, API_VERSION, API_ENDPOINT
+from config import AZURE_OPENAI_API_KEY, API_VERSION, API_ENDPOINT, API_MODEL_MINI, API_MODEL
+
 
 class PlaylistChat:
 
     def __init__(self):
         self.client = AzureOpenAI(
                 api_version = API_VERSION,
-                azure_endpoint = API_ENDPOINT
+                azure_endpoint = API_ENDPOINT,
+                api_key = AZURE_OPENAI_API_KEY
         )
 
     def receive_message(self, message):
-        print(message)
+        completion = self.client.chat.completions.create(
+                model = API_MODEL_MINI,
+                messages=[
+                    { "role":"user", "content":message },
+                        ],
+        )
+
+        return completion.choices[0].message.content
 
