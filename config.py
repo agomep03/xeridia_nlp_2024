0fd5e7e5fdd068ed9dbd02d5208b44dc9cc13ea4
alicia <agomep03@estudiantes.unileon.es>
2024-11-29 10:10:05 +0100

    Create global variables and gitignore

--- config.py
+++ config.py
@@ -1,1 +1,7 @@
-#TODO: save keys and configurations
+OPENAI_API_KEY = "7MoKzuuPAla0Cl3RYSM2aHjTAmkVopeVC4VhHCCdLPGDbAg0Vk4OJQQJ99AKACYeBjFXJ3w3AAABACOGrjrr"
+API_VERSION = "2024-10-01-preview"
+API_ENDPOINT = "https://practicas-2024.openai.azure.com/"
+API_LOCATION = "eastus"
+
+API_MODEL_MINI = "gpt-4o-mini"
+API_MODEL = "gpt-4o"
