0c7c9638e88c5beaeb65a247feede3f1140b8f1e
alicia <agomep03@estudiantes.unileon.es>
2024-11-29 10:29:25 +0100

    base chat

--- chat/playlist_chat.py
+++ chat/playlist_chat.py
@@ -1,1 +1,17 @@
-#TODO: class of chat to a list of songs of a playlist
+import sys
+import os
+
+from openai import AzureOpenAI
+from config import AZURE_OPENAI_API_KEY, API_VERSION, API_ENDPOINT
+
+class PlaylistChat:
+
+    def __init__(self):
+        self.client = AzureOpenAI(
+                api_version = API_VERSION,
+                azure_endpoint = API_ENDPOINT
+        )
+
+    def receive_message(self, message):
+        print(message)
+
