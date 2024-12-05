4c82c992c4af849f04b55cc06cea8370bc313bd4
mcastm12 <mcastm12@estudiantes.unileon.es>
2024-12-05 10:14:18 +0100

    Cambios de reqs

--- process_input/prompt_classifier.py
+++ process_input/prompt_classifier.py
@@ -1,5 +1,6 @@
 import joblib
 import os
+from chat.song_lyrics import SongLyricsFetcher
 
 print(os.getcwd())
 pipeline = joblib.load('process_input/text_classification_pipeline.pkl')
@@ -10,4 +11,16 @@ text = input("Introduzca su consulta:\n")
 
 predictions = pipeline.predict([text])
 print("Predicciones:", predictions)
+
+if(predictions[0] == 0):
+    print("artista")
+    
+elif(predictions[0] == 1):
+    print("consultar_informacion")
+
+elif(predictions[0] == 2):
+    print("crear_playlist")
+
+elif(predictions[0] == 3):
+    SongLyricsFetcher.get_lyrics([text])
 
