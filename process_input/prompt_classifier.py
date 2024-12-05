f7a6e67a9cc4793faad82cee7f22c57afb37666f
mcastm12 <mcastm12@estudiantes.unileon.es>
2024-12-05 10:01:30 +0100

    Modificacion de requirements y creacion de pipeline

--- process_input/prompt_classifier.py
+++ process_input/prompt_classifier.py
@@ -1,1 +1,13 @@
-#TODO: class to classify the input of the user into "recommend", "consult", "create playlist" 
+import joblib
+import os
+
+print(os.getcwd())
+pipeline = joblib.load('process_input/text_classification_pipeline.pkl')
+
+dictionary = {"artista": 0, "consultar_informacion": 1, "crear_playlist": 2}
+
+text = input("Introduzca su consulta:\n")
+
+predictions = pipeline.predict([text])
+print("Predicciones:", predictions)
+
