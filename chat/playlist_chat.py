1df5265b11404cb8345351251b39d53c3c98c8c8
alicia <agomep03@estudiantes.unileon.es>
2024-12-13 13:53:09 +0100

    Al pedir una playlist se predice que generos musicales va a preferir el ususario

--- chat/playlist_chat.py
+++ chat/playlist_chat.py
@@ -1,5 +1,6 @@
 import re
 import random
+import joblib
 
 from openai import AzureOpenAI
 from config import AZURE_OPENAI_API_KEY, API_VERSION, API_ENDPOINT, API_MODEL_MINI, API_MODEL
@@ -26,6 +27,9 @@ def parse_response(response):
         
         return {"description":description, "title":title, "list":list_of_songs}
 
+MUSICAL_GENDERS = ["Pop", "Rock","Hip-Hop","EDM","Indie","Jazz","Soul","Reggaetón","Música Clásica","Folk","R&B","Canciones acústicas","Lo-fi",
+"Música instrumental","Música ambiental","Música latina","Funk","Música tropical","Bossa Nova","Dance"]
+
 class PlaylistChat:
 
     def __init__(self):
@@ -40,7 +44,8 @@ class PlaylistChat:
                             "[TITLEPLAYLIST]: <título de la playlist>. "
                             "[PLAYLIST]: <canción 1>, <canción 2>, <canción 3>, ..."}
         self.sentiment = SentimentAnalysis()
-    
+
+        self.musical_gender_model, self.musical_gender_vectorizer = joblib.load('utils/musical_gender_model.pkl')
     
 
     def receive_message(self, message):
@@ -48,6 +53,9 @@ class PlaylistChat:
         keywords = extract_keywords(message)
         keywords = keywords.split(" ")
 
+        for word in self.search_musical_gender(" ".join(keywords)):
+            keywords.append(word)
+                    
         # Add sentimient
         sentiment = self.sentiment.analyze_sentiment(message=message)
         if 'Estado de ánimo' in sentiment:
@@ -63,11 +71,22 @@ class PlaylistChat:
 
         prompt="El usuario ha pedido: "+message+" Se le ha creado una playlist con las canciones: "+" ,".join(songsNames)
         response = self.create_response(prompt)
+
         print(response)
 
         create_playlist(songs=songs, title=response['title'], description=response['description'])
 
         return response
+    
+    def search_musical_gender(self, message):
+        embbeded = self.musical_gender_vectorizer.transform([message])
+        preds = self.musical_gender_model.predict(embbeded)[0]
+        genders = []
+        for i in range(len(preds)):
+            if preds[i]:
+                genders.append(MUSICAL_GENDERS[i])
+
+        return genders
 
     
     def search_songs(self, keywords, numberSongsToSearch=60):
